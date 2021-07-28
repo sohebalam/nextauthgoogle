e7fd64ffc75bb2cf559fafb37caf2e6c4ee858d9
soheb alam <sohebalam@hotmail.com>
2021-07-28 19:15:31 +0100

    first

---
+++ pages/index.js
@@ -0,0 +1,32 @@
+import styles from "../styles/Home.module.css"
+import { getSession, signIn, signOut } from "next-auth/client"
+
+export default function Home({ session }) {
+  console.log(session)
+  return (
+    <div className={styles.container}>
+      {session ? (
+        <button onClick={() => signOut()}>Sign out</button>
+      ) : (
+        <button onClick={() => signIn()}>Sign in</button>
+      )}
+      {session && (
+        <div>
+          <p>Signed in as {session.user.email}</p>
+          <p>Name: {session.user.name}</p>
+          <img src={session.user.image} alt={session.user.name} />
+        </div>
+      )}
+    </div>
+  )
+}
+
+export const getServerSideProps = async (context) => {
+  const session = await getSession(context)
+
+  return {
+    props: {
+      session,
+    },
+  }
+}
